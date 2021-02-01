975761e20508cf9f06bbc87acb548ec9b26e808b
Andrea <corro.andrea.1995@gmail.com>
2021-02-01 12:59:01 +0100

    lista utenti

---
+++ js/script.js
@@ -0,0 +1,65 @@
+var nuovo = new Vue({
+  el: '#chat-view',
+  data: {
+    utenti: [
+      {
+        nome: 'utente1',
+        avatar: 'img/avatar_2.jpg'
+      },
+      {
+        nome: 'utente2',
+        avatar: 'img/avatar_3.jpg'
+      },
+      {
+        nome: 'utente3',
+        avatar: 'img/avatar_4.jpg'
+      },
+      {
+        nome: 'utente4',
+        avatar: 'img/avatar_5.jpg'
+      },
+      {
+        nome: 'utente5',
+        avatar: 'img/avatar_6.jpg'
+      },
+      {
+        nome: 'utente6',
+        avatar: 'img/avatar_7.jpg'
+      },
+      {
+        nome: 'utente7',
+        avatar: 'img/avatar_8.jpg'
+      },
+      {
+        nome: 'utente8',
+        avatar: 'img/avatar_2.jpg'
+      },
+      {
+        nome: 'utente9',
+        avatar: 'img/avatar_3.jpg'
+      },
+      {
+        nome: 'utente10',
+        avatar: 'img/avatar_4.jpg'
+      },
+      {
+        nome: 'utente11',
+        avatar: 'img/avatar_5.jpg'
+      },
+      {
+        nome: 'utente12',
+        avatar: 'img/avatar_6.jpg'
+      },
+      {
+        nome: 'utente13',
+        avatar: 'img/avatar_7.jpg'
+      },
+      {
+        nome: 'utente14',
+        avatar: 'img/avatar_8.jpg'
+      }
+    ]
+  },
+  methods: {
+  }
+});
